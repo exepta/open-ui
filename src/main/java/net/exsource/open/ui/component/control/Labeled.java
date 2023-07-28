2496622ec8ebd45533e7e106145b850a52bf2ee1
danielramke <daniel.ramke@lyth.de>
2023-07-28 12:25:09 +0200

    Labeled was update for his needed functions

--- src/main/java/net/exsource/open/ui/component/control/Labeled.java
+++ src/main/java/net/exsource/open/ui/component/control/Labeled.java
@@ -1,5 +1,10 @@
 package net.exsource.open.ui.component.control;
 
+import net.exsource.open.enums.geometry.Alignment;
+import net.exsource.open.ui.component.Component;
+import net.exsource.open.ui.font.Font;
+import net.exsource.open.ui.style.generic.FontLook;
+
 public interface Labeled {
 
     void setText(String text);
@@ -9,5 +14,41 @@ public interface Labeled {
     void setWarping(boolean warping);
 
     boolean isWarping();
+
+    void update();
+
+    boolean isUpdated();
+
+    int getMaxRows();
+
+    void setFont(String name);
+
+    void setFont(Font font);
+
+    Font getFont();
+
+    void setLook(FontLook look);
+
+    FontLook getLook();
+
+    double getTextPositionX();
+
+    double getTextPositionY();
+
+    void setTextAbsolute(double x, double y);
+
+    double getTextAbsoluteX();
+
+    double getTextAbsoluteY();
+
+    double getLableWidth();
+
+    double getLableHeight();
+
+    void setTextAlignment(Alignment alignment);
+
+    Alignment getTextAlignment();
+
+    Component getHolder();
 
 }
